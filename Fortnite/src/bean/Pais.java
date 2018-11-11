3247d0e78144a96e6e1a5ccf64ebc4b4d5c69512
Alex <alex.amac358@gmail.com>
2018-11-11 14:29:17 -0500

    Se documento
    Se agreagaron comentarios a los atributos

--- Fortnite/src/bean/Pais.java
+++ Fortnite/src/bean/Pais.java
@@ -1,12 +1,24 @@
 package bean;
 
 public class Pais {
+	/* Identificador del Pais */
 	public int idpais;
+	/* Nombre del Pais */
 	public String nombrePais;
 	
+	// GET / SET
+	
+	/**
+	 * Metodo que obtiene el Identificador del Pais
+	 * @return idpais identificador del pais, tipo int
+	 */
 	public int getIdpais() {
 		return idpais;
 	}
+	/**
+	 * Metodo que envia el Identificador del Pais
+	 * @param idpais identificador del pais, tipo int
+	 */
 	public void setIdpais(int idpais) {
 		this.idpais = idpais;
 	}
